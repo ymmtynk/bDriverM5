e7543455e4c7fef24f8917fdeeeadfb19cd1eb7c
Takashi Yamamoto <ymmt_jacog@me.com>
2022-04-30 16:21:06 +0900

    bDriverM5 firmware for M5StickCPlus

---
+++ bDriverM5p/bDriverM5p.ino
@@ -0,0 +1,210 @@
+#include <M5StickCPlus.h>
+#include <Preferences.h>
+
+Preferences preferences;
+
+// Preferenceで覚えておくもの
+const char  PREF_KEY_AUTOCONNECT[]    = "AutoConnect";        // AutoConnect On/Off           bool
+const char  PREF_KEY_LASTCONNAME[]    = "LastConName";        // LastConnected bCore Name     String
+
+
+#define  STATE_NOT_READY    0               // 起動処理前
+#define  STATE_READY        1               // 起動完了、Scan、接続待ち
+#define  STATE_SCANNING     2               // Scan中
+#define  STATE_CONNECTED    3               // 接続中
+
+int     state;                              // 状態管理
+bool    autoConnect;                        // 自動接続オンオフ管理フラグ
+String  lastConName;                        // 自動接続する場合の接続相手名称
+
+#define NUM_OF_SCAN_COUNT   100             // Scan時間のカウントダウン x 1/10[sec]
+int     scancounter;                        // 
+
+int     selection;                          // UI操作の上下移動の位置
+
+float   batteryVoltage = -1.0f;             // 電源電圧
+
+
+void    getLastConName()
+{
+  if (autoConnect == true) {
+    lastConName = preferences.getString(PREF_KEY_LASTCONNAME);
+    Serial.println(" Load Auto Connect Target [" + lastConName + "]\r\n");   
+  }
+  
+  return;
+}
+
+void    setLastConName(String nameStr)
+{
+  if (autoConnect == true) {
+    lastConName = nameStr;
+    preferences.putString(PREF_KEY_LASTCONNAME, lastConName);
+    Serial.println(" Save Auto Connect Target [" + lastConName + "]\r\n");       
+  }
+   
+  return;
+}
+
+void  switch5Vout(bool onoff)
+{
+  if (onoff == false) {
+    Serial.println("5V Out Off");
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x12);
+    Wire1.endTransmission();
+    Wire1.requestFrom(0x34, 1);
+    uint8_t state = Wire1.read() & ~(1 << 6);
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x12);
+    Wire1.write(state);
+    Wire1.endTransmission();
+  } else{
+    Serial.println("5V Out On");
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x12);
+    Wire1.endTransmission();
+    Wire1.requestFrom(0x34, 1);
+    uint8_t state = Wire1.read() | (1 << 6);
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x12);
+    Wire1.write(state);
+    Wire1.endTransmission();
+  }
+
+  return;
+}
+
+void  switchRTC(bool onoff)
+{
+  if (onoff == false) {
+    Serial.println("RTC Off");
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x35);
+    Wire1.endTransmission();
+    Wire1.requestFrom(0x34, 1);
+    uint8_t state = Wire1.read() & ~(1 << 7);
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x35);
+    Wire1.write(state);
+    Wire1.endTransmission();
+  } else {
+    Serial.println("RTC_On");
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x35);
+    Wire1.endTransmission();
+    Wire1.requestFrom(0x34, 1);
+    uint8_t state = Wire1.read() | (1 << 7);
+    Wire1.beginTransmission(0x34);
+    Wire1.write(0x35);
+    Wire1.write(state);
+    Wire1.endTransmission();  
+  }
+  
+  return;
+}
+
+
+void setup() 
+{
+  state = STATE_NOT_READY;
+  
+  M5.begin();
+  M5.Beep.setBeep(4000, 100);
+
+  // Low Power 
+  setCpuFrequencyMhz(80);       // 240MHz -> 80MHz
+  M5.Axp.ScreenBreath(8);       // LCD Backlight
+  switchRTC(false);             // RTC Off
+  switch5Vout(false);           // 5V output Off
+  
+  M5.Lcd.setRotation(1);
+  M5.Lcd.setTextSize(1);
+  
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println("> bCore BLE Controller");
+  
+  preferences.begin("bDriver", false);
+  autoConnect = preferences.getBool(PREF_KEY_AUTOCONNECT);
+  getLastConName();
+  
+  selection = 0;
+
+  init_BTN();           // ボタン周りの初期化
+  update_LCD();         // LCD周りの初期化
+  init_LED();           // LED周りの初期化
+  init_multiADC();      // 外部接続のADCの初期化
+
+  Serial.println("> Ready");
+  state = STATE_READY;
+  
+  // BLE Scanは処理をブロックするのでボタン操作、表示更新の処理は別コアで行う
+  Serial.println("> Start SubTask");
+  xTaskCreatePinnedToCore(updateSubTask, "updateSubTask", 4096, NULL, 1, NULL, 0);
+
+  Serial.println("> Init BLE");  
+  init_bCoreBLE();  // BLE初期化
+  
+  return;
+}
+
+
+void loop() 
+{
+  //Serial.printf("loop(%d)\r\n", counter);
+  if (state == STATE_READY) {
+    checkAutoConnect();
+    checkManualConnect();
+    delay(100);    
+  } else 
+  if (state == STATE_SCANNING) {
+    Serial.println("> bCoreBLE_startScan(10)");
+    bCoreBLE_startScan(10);         // この命令は処理をブロックする！
+    Serial.println("> end of scan.");
+    state = STATE_READY;
+
+    checkAutoConnect();
+    checkManualConnect();
+    delay(100);
+  } else 
+  if (state == STATE_CONNECTED) {
+    byte    burstCommandBuffer[7];    // Mot1, Mot2, LED, Servo1, Servo2, Servo3, Servo4
+    burstCommandBuffer[0] = get_pwmValue(0);
+    burstCommandBuffer[1] = get_pwmValue(1);
+    burstCommandBuffer[2] = get_portValue();
+    burstCommandBuffer[3] = get_servoValue(0);
+    burstCommandBuffer[4] = get_servoValue(1);
+    burstCommandBuffer[5] = get_servoValue(2);
+    burstCommandBuffer[6] = get_servoValue(3);    
+    bCoreBLE_sendBurstCommand(burstCommandBuffer);
+    delay(50);
+    
+    // Update Batteru Voltage Info
+    batteryVoltage = bCoreBLE_getBatteryVoltage();
+    delay(50);
+  } else {
+    delay(100);
+  }
+
+  //Serial.printf("%d[sec]\r\n", millis()/1000);
+  
+  return;
+}
+
+
+void updateSubTask(void * pvParameters) 
+{
+  while(1) {
+    update_multiADC();
+    
+    update_BTN();          // ボタンの状態更新
+    update_LED();          // Status LEDの点滅更新
+    update_LCD();          // LCD表示後進
+    
+    delay(100);
+  }
+  
+  return;
+}
---
+++ bDriverM5p/multiADC.ino
@@ -0,0 +1,226 @@
+// NCD9830 https://www.onsemi.com/pub/Collateral/NCD9830-D.PDF
+#include <Wire.h>
+
+#define     I2C_SCK     26
+#define     I2C_SDA      0
+#define     NCD9830_ADDRESS     0x48
+
+byte    i2cAdcBuff[8];
+
+
+bool    port[4];
+float   rollAtt, pitchAtt, yawAtt; 
+
+extern struct bCoreSetting    bStg;
+
+
+void  init_multiADC()
+{
+  Wire.begin(I2C_SDA, I2C_SCK);
+  
+  return;
+}
+
+bool  get_I2C_ADCResult(int ch, byte* adcdata)
+{
+  bool result = false;
+  byte command;
+
+  int num;
+  
+  switch (ch) {
+    case 0 :  command = 0x84; break;
+    case 1 :  command = 0xc4; break;
+    case 2 :  command = 0x94; break;
+    case 3 :  command = 0xd4; break;
+    case 4 :  command = 0xa4; break;
+    case 5 :  command = 0xe4; break;
+    case 6 :  command = 0xb4; break;
+    case 7 :  command = 0xf4; break;
+  }
+  Wire.beginTransmission(NCD9830_ADDRESS);
+  Wire.write(&command, 1);
+    
+  if (Wire.endTransmission(false) != 0) {
+    return  result;
+  }
+
+  if (Wire.requestFrom(NCD9830_ADDRESS, 1) != 1) {
+    return  result;
+  }
+    
+  num = 0;
+  while (Wire.available()) {
+    *adcdata = Wire.read();
+    num++;
+  }
+
+  if (num == 1) {
+    ;
+  } else {
+        return  result;        
+  }
+  
+  result = true;
+  
+  return  result;
+}
+
+void    update_multiADC()
+{
+  int i;
+
+  for (i = 0; i<8; i++) {
+    get_I2C_ADCResult(i, &(i2cAdcBuff[i]));
+  }
+  
+  return;
+}
+
+byte     get_adc_value(int ch)
+//byte     get_I2C_adc_value(int ch)
+{
+  return  i2cAdcBuff[ch];
+}
+
+
+byte    get_control_value(int sourceid)
+{
+  byte  result;
+
+  switch (sourceid) {
+    case 0:
+      result = get_adc_value(0);
+      break;
+    case 1:
+      result = get_adc_value(1);
+      break;
+    case 2:
+      result = get_adc_value(2);
+      break;
+    case 3:
+      result = get_adc_value(5);
+      break;
+    case 4:
+      result = get_adc_value(6);
+      break;
+    case 5:     
+      result = get_adc_value(7);
+      break;
+    case 6:     // Roll
+      //result = (byte)(128.0 - rollAtt*(128.0f/180.0f));
+      result = 128;
+      break;
+    case 7:     // Pitch
+      //result = (byte)(128.0 - pitchAtt*(128.0f/180.0f));;
+      result = 128;
+      break;
+    case 8:     // Yaw
+      //result = (byte)(128.0 - yawAtt*(128.0f/180.0f));;
+      result = 128;
+      break;
+    default:
+      result = 128;
+      break;
+  }
+  
+  return  result;
+}
+
+
+byte    get_servoValue(int ch)
+{
+  byte result = 0;
+  if (ch == 0) {
+    result = get_control_value(bStg.source[2]);
+  } else 
+  if (ch == 1) {
+    result = get_control_value(bStg.source[3]);
+  } else 
+  if (ch == 2) {
+    result = get_control_value(bStg.source[4]);
+  } else 
+  if (ch == 3) {
+    result = get_control_value(bStg.source[5]);
+  }
+
+  if (bStg.flip[ch+2]) {
+    result = 255 - result;
+  }
+  
+  return  result;
+}
+
+byte    get_pwmValue(int ch)
+{
+  byte result = 0;
+  if (ch == 0) {
+    result = get_control_value(bStg.source[0]);
+  } else 
+  if (ch == 1) {
+    result = get_control_value(bStg.source[1]);
+  }
+  
+  if (bStg.flip[ch]) {
+    result = 255 - result;
+  }
+
+  // モーターコントロールは原点付近に不感帯を設けないと
+  // モーターが回らないのに無駄に電流が流れてしまう
+  if (((128-10) < result) && (result < (128+10))) {
+    result = 128;
+  }
+  
+  return  result;
+}
+
+byte    update_swValue()
+{
+    byte result = 0x00;
+    byte adc3 = get_adc_value(3);
+    byte adc4 = get_adc_value(4);
+
+    if (adc3 > 213) {
+    } else 
+    if (adc3 > 149) {
+      result += 0x02;
+    } else 
+    if (adc3 > 115) {
+      result += 0x01;
+    } else {
+      result += 0x03;
+    }
+    if (adc4 > 213) {
+    } else 
+    if (adc4 > 149) {
+      result += 0x08;
+    } else 
+    if (adc4 > 115) {
+      result += 0x04;
+    } else {
+      result += 0x0C;
+    }
+    
+    return  result;
+}
+
+byte    get_portValue()
+{
+  byte result = 0x00;
+  
+  if (port[0] == true) {
+    result += 0x01;
+  }
+  if (port[1] == true) {
+    result += 0x02;
+  }
+  if (port[2] == true) {
+    result += 0x04;
+  }
+  if (port[3] == true) {
+    result += 0x08;
+  }
+  
+  return    result;
+}
+
